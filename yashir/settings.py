5d409674d84a054223d7470249aa36981fa0bdbf
Mohand Abdo <mohand30466@gmail.com>
2022-06-21 23:59:13 +0300

    add allow host

--- yashir/settings.py
+++ yashir/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['directltd.herokuapp.com']
 
 
 # Application definition
